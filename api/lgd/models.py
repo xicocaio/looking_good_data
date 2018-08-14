1bc7605e2acb1e7015bd9faae915ba5a573fbc4c
Francisco Paiva <xicocaio@gmail.com>
2018-08-14 17:52:55 -0300

    Add DB setup

--- api/lgd/models.py
+++ api/lgd/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Order(models.Model):
+	user_id = models.IntegerField()
+	date = models.DateTimeField()
+	subtotal = models.FloatField()
+	discount = models.FloatField(default=0)
+	total = models.FloatField()
+	status = models.CharField(max_length=200)
+	payment_type = models.CharField(max_length=200)
+	shipping_cost = models.FloatField()
+	shipping_service = models.CharField(max_length=200)
+	address_city = models.CharField(max_length=100)
+	address_state = models.CharField(max_length=10)
+	utm_source_medium = models.CharField(max_length=200)
+	device_type = models.CharField(max_length=200)
+
+	def __str__(self):
+		return str(self.id)
+
+class OrderItem(models.Model):
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	sku = models.CharField(max_length=200)
+	quantity = models.IntegerField(default=1)
+	code_color = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.sku
+
+	class Meta:
+		db_table = 'lgd_order_item'
+
