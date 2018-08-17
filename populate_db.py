33bd36ea3d97dd7b0c7f7d2af954576f9007351c
Francisco Paiva <xicocaio@gmail.com>
2018-08-17 17:15:31 -0300

    Normalize device_type data to lowercase

--- populate_db.py
+++ populate_db.py
@@ -7,11 +7,12 @@ con = sa.create_engine('sqlite:///db.sqlite3')
 
 # read order data from CSV
 # (na_filter=False) use NULL as blank string, avoids issues inserting on DB
-df_order = pd.read_csv('orders.csv', sep=';', index_col=0, na_filter=False)
-df_order_item = pd.read_csv('order_items.csv', sep=';', index_col=0, na_filter=False)
+df_order = pd.read_csv('orders.csv', sep=';', index_col=0, na_filter=False, skipinitialspace=True)
+df_order_item = pd.read_csv('order_items.csv', sep=';', index_col=0, na_filter=False, skipinitialspace=True)
 
 # convert date column to specific format
 df_order['date'] = pd.to_datetime(df_order['date'], format="%d/%m/%Y %H:%M")
+df_order['device_type'] = df_order['device_type'].str.lower()
 
 # load data to DB
 df_order.to_sql(name='lgd_order', if_exists='append', con=con)
